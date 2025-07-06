c22f31ce7510dedcb3b0df896440e1e9524b759c
Peter-Frank Spierenburg <spierepf@hotmail.com>
2025-07-06 12:56:14 -0300

    Added unittest.mock to test.py

--- SwitchboardOperatorGame/test.py
+++ SwitchboardOperatorGame/test.py
@@ -7,6 +7,11 @@ if 'unittest' not in os.listdir(install.libpath):
 
     mip.install('unittest', target=install.libpath)
 
+if 'mock.py' not in os.listdir(install.libpath+"/unittest"):
+    import mip
+
+    mip.install('github:spierepf/mip_packages/umock/package.json', target=install.libpath)
+
 import unittest
 # noinspection PyUnresolvedReferences
 from tests.test_mock_pin import TestMockPin
